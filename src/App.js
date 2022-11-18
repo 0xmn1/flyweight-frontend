d31053b82df92ec72c6fdaf647fb17573565daea
m n <116815127+0xmn1@users.noreply.github.com>
2022-11-18 18:46:45 +1100

    Expand erc20 list

--- src/App.js
+++ src/App.js
@@ -14,35 +14,23 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import Card from 'react-bootstrap/Card';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import coinSymbols from './coin-symbols.json';
+import ordersContractAbi from './orders-smart-contract-abi.json';
 import { ethers } from 'ethers';
+import { HourglassSplit } from 'react-bootstrap-icons';
 
-const ordersContractAddress = '0x4CE49b22BDB1f1f41a19160FF907530e62dD3912';
-const ordersContractAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"orderId","type":"uint256"}],"name":"OrderExecuted","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"orderId","type":"uint256"}],"name":"OrderTriggered","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"timestamp","type":"uint256"},{"indexed":false,"internalType":"string","name":"symbol","type":"string"},{"indexed":false,"internalType":"string","name":"oldPrice","type":"string"},{"indexed":false,"internalType":"string","name":"newPrice","type":"string"}],"name":"PriceUpdated","type":"event"},{"inputs":[{"internalType":"string","name":"tokenIn","type":"string"},{"internalType":"string","name":"tokenOut","type":"string"},{"internalType":"string","name":"tokenInTriggerPrice","type":"string"},{"internalType":"enum Flyweight.OrderTriggerDirection","name":"direction","type":"uint8"},{"internalType":"uint256","name":"tokenInAmount","type":"uint256"}],"name":"addNewOrder","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"orders","outputs":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"address","name":"owner","type":"address"},{"internalType":"enum Flyweight.OrderState","name":"orderState","type":"uint8"},{"internalType":"string","name":"tokenIn","type":"string"},{"internalType":"string","name":"tokenOut","type":"string"},{"internalType":"string","name":"tokenInTriggerPrice","type":"string"},{"internalType":"enum Flyweight.OrderTriggerDirection","name":"direction","type":"uint8"},{"internalType":"uint256","name":"tokenInAmount","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ordersCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"prices","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"components":[{"internalType":"string","name":"symbol","type":"string"},{"internalType":"string","name":"price","type":"string"}],"internalType":"struct Flyweight.NewPriceItem[]","name":"newPriceItems","type":"tuple[]"},{"internalType":"uint256[]","name":"newTriggeredOrderIds","type":"uint256[]"}],"name":"storePricesAndProcessTriggeredOrderIds","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"tokenAddresses","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}];
-const uniTokenAddress = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984';
-const wethTokenAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
+const ordersContractAddress = '0x0180efB63Ae8C1607443B4Af87fF8ed49848Bb25';
 const erc20Abi = [{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}];
 
 let signer = null;
 let ordersContract = null;
-let uniTokenContract = null;
-let wethTokenContract = null;
-
-const tokenContracts = {
-  UNI: null,
-  WETH: null
-};
 
 const initContracts = () => {
   const signer = provider.getSigner();
   ordersContract = new ethers.Contract(ordersContractAddress, ordersContractAbi, signer);
-  tokenContracts.UNI = new ethers.Contract(uniTokenAddress, erc20Abi, signer);
-  tokenContracts.WETH = new ethers.Contract(wethTokenAddress, erc20Abi, signer);
 };
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-provider.send('eth_requestAccounts', [])
-  .then(initContracts)
-  .catch(console.error);
 
 class App extends React.Component {
   constructor() {
@@ -53,55 +41,25 @@ class App extends React.Component {
       tokenOutSymbol: 'WETH',
       triggerDirection: 2,
       triggerPrice: '0.02',
-      userMessages: {
-        info: 'Blockchain transaction is now submitting (ethereum block approval time takes ~15 secs).',
-        warning: 'No effect has taken place (the blockchain transaction was reverted).'
-      }
+      whitelistedCoinSymbols: [],
+      alertMsgPrimary: null,
+      alertMsgSecondary: null,
+      alertVariant: null
     };
   }
 
-  addOrder = async () => {
-    const tokenInDecimals = await tokenContracts[this.state.tokenInSymbol].functions.decimals();
-    const tokenOutDecimals = await tokenContracts[this.state.tokenOutSymbol].functions.decimals();
-    const tokenInAmount = new Big(`${this.state.tokenInDecimalAmount}e${tokenInDecimals}`);
-    const tokenInAmountStr = tokenInAmount.toString();
+  async componentDidMount() {
+    provider.send('eth_requestAccounts', [])
+      .then(async () => {
+        initContracts();
+        await this.setWhitelistedCoinSymbols();
+      })
+      .catch(console.error);
+  }
 
-    console.log(this.state.tokenInSymbol);
-    console.log(this.state.tokenOutSymbol);
-    console.log(this.state.triggerPrice);
-    console.log(this.state.triggerDirection);
-    console.log(tokenInAmount.toString());
-
-    try {
-      const txNewOrder = await ordersContract.functions.addNewOrder(
-        this.state.tokenInSymbol,
-        this.state.tokenOutSymbol,
-        this.state.triggerPrice,
-        this.state.triggerDirection,
-        tokenInAmountStr
-      );
-
-      const txNewOrderReceipt = await txNewOrder.wait();
-      if (txNewOrderReceipt.status === 0) {
-        throw txNewOrderReceipt;
-      }
-
-      alert('Order added successfully. You may now deposit coins into the contract for your order');
-
-      const metamaskSigner = provider.getSigner();
-      const txDeposit = await tokenContracts[this.state.tokenInSymbol]
-        .transfer(ordersContractAddress, tokenInAmountStr);
-
-      const txDepositReceipt = await txDeposit.wait();
-      if (txDepositReceipt.status === 0) {
-        throw txDepositReceipt;
-      }
-
-      alert('Coins deposited successfully. Your order is now live will be triggered when the conditions are met.');
-    } catch (err) {
-      console.error(err);
-      alert('Sorry, something went wrong. Please try again');
-    }
+  setWhitelistedCoinSymbols = async () => {
+    const res = await ordersContract.functions.getWhitelistedSymbols(coinSymbols);
+    this.setState({ whitelistedCoinSymbols: res[0] });
   };
 
   getOrderDirectionLabel = orderDirectionEnum => {
@@ -115,6 +73,68 @@ class App extends React.Component {
         return 'above';
     }
   };
+
+  tryAddOrder = async () => {
+    if (this.state.tokenInDecimalAmount <= 0) {
+      this.showAlert('warning', 'Please select a postive number of tokens to swap.', `Your order is currently configured to swap "${this.state.tokenInDecimalAmount}" tokens`);
+    } else if (this.state.tokenInSymbol === this.state.tokenOutSymbol) {
+      this.showAlert('warning', 'Please select 2 different pairs of tokens to swap.', `Your order is currently configured to swap "${this.state.tokenInSymbol}" to "${this.state.tokenOutSymbol}"`);
+    } else if (this.state.triggerPrice < 0) {
+      this.showAlert('warning', 'Please select a non-negative trigger price.', `Your order is currently configured to trigger at "${this.state.triggerPrice} $USD"`);
+    } else {
+      this.showAlert(null, null, null);
+      await this.addOrder();
+    }
+  };
+
+  addOrder = async () => {
+    const signer = provider.getSigner();
+    const tokenInAddresses = await ordersContract.functions.tryGetTokenAddress(this.state.tokenInSymbol);
+    const tokenOutAddresses = await ordersContract.functions.tryGetTokenAddress(this.state.tokenOutSymbol);
+    const tokenInAddress = tokenInAddresses[0];
+    const tokenOutAddress = tokenOutAddresses[0];
+    const tokenInContract = new ethers.Contract(tokenInAddress, erc20Abi, signer);
+    const tokenOutContract = new ethers.Contract(tokenOutAddress, erc20Abi, signer);
+
+    const tokenInDecimals = await tokenInContract.functions.decimals();
+    const tokenOutDecimals = await tokenOutContract.functions.decimals();
+    const tokenInAmount = new Big(`${this.state.tokenInDecimalAmount}e${tokenInDecimals}`);
+    const tokenInAmountStr = tokenInAmount.toString();
+
+    try {
+      this.showAlert('primary', 'Please confirm transaction [1] of [2] in metamask.', 'This will add your order to the ethereum smart contract. This transaction transfers data only and does not transfer any coins.');
+      const txNewOrder = await ordersContract.functions.addNewOrder(
+        this.state.tokenInSymbol,
+        this.state.tokenOutSymbol,
+        this.state.triggerPrice,
+        this.state.triggerDirection,
+        tokenInAmountStr
+      );
+
+      this.showAlert('info', 'Processing transaction, beep boop...', 'Awaiting network approval (ethereum blocks take ~15 secs, unless there is degen monkey nft minting going on, or something worse than monkeys..)');
+      const txNewOrderReceipt = await txNewOrder.wait();
+      if (txNewOrderReceipt.status === 0) {
+        throw txNewOrderReceipt;
+      }
+
+      this.showAlert('success', 'Please confirm transaction [2] of [2] in metamask.', 'This allows you to deposit the coins for your order to automatically swap via uniswap. Users can cancel untriggered orders at any time to get their coins sent back');
+      const metamaskSigner = provider.getSigner();
+      const txDeposit = await tokenInContract.transfer(ordersContractAddress, tokenInAmountStr);
+
+      this.showAlert('info', 'Finalizing your order now, boop beep...', 'Awaiting network approval. Remember the bull market days when gas was 2000+ gwei? Wen zk sync token');
+      const txDepositReceipt = await txDeposit.wait();
+      if (txDepositReceipt.status === 0) {
+        throw txDepositReceipt;
+      }
+
+      this.showAlert('success', 'Neat! Your order is now live, and will be triggered when your conditions are met.', 'This is a decentralized protocol: users can refund coin deposits at any time, yay. Goodluck in the casino comrade (you may now close this box/browser tab)');
+    } catch (err) {
+      console.error(err);
+      this.showAlert('warning', `Order cancelled (${err.reason})`);
+    }
+  };
+
+  showAlert = (alertVariant, alertMsgPrimary, alertMsgSecondary) => this.setState({ alertVariant, alertMsgPrimary, alertMsgSecondary });
 
   render() {
     return (
@@ -140,14 +160,17 @@ class App extends React.Component {
         <Container>
           <Row>
             <Col>
-              <Alert key="info" variant="info" show={this.state.userMessages.info}>
-                <div>{this.state.userMessages.info}</div>
-                <Alert.Link href="#" target="_blank">Frequently asked questions</Alert.Link>
-              </Alert>
-              <Alert key="warning" variant="warning" show={this.state.userMessages.warning}>
-                <div>We're sorry, something went wrong.</div>
-                <div>{this.state.userMessages.warning}</div>
-                <Alert.Link href="#" target="_blank">Frequently asked questions</Alert.Link>
+              <Alert variant={this.state.alertVariant} show={this.state.alertMsgPrimary} onClose={() => this.showAlert(null, null, null)} className='wrapper-alert' dismissible>
+                <h5 className="d-flex align-items-center mb-0">
+                <div className={this.state.alertVariant === 'info' ? 'anim-pulsing-circle anim-pulse' : null}></div>
+                  <div>{this.state.alertMsgPrimary}</div>
+                </h5>
+                <div>
+                  <small>{this.state.alertMsgSecondary}</small>
+                </div>
+                <Alert.Link href="#" target="_blank">
+                  <small class="text-muted">Frequently asked questions</small>
+                </Alert.Link>
               </Alert>
             </Col>
           </Row>
@@ -166,30 +189,35 @@ class App extends React.Component {
                   <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                       <Form.Text>I want to swap...</Form.Text>
-                      <Stack direction="horizontal" gap={1}>                  
-                        <Form.Control type="number" placeholder="token in amount" defaultValue={this.state.tokenInDecimalAmount} onChange={e => this.setState({ tokenInAmount: e.target.value })} />
+                      <Stack direction="horizontal" gap={1} className="mb-2">
+                        <Form.Control type="number" placeholder="example: 10.0001" defaultValue={this.state.tokenInDecimalAmount} onChange={e => this.setState({ tokenInDecimalAmount: e.target.value })} />
                         <DropdownButton title={this.state.tokenInSymbol} onSelect={(tokenInSymbol, _) => this.setState({ tokenInSymbol })} variant="dark">
-                          <Dropdown.Item eventKey="UNI" active={this.state.tokenInSymbol === 'UNI'}>UNI (Uniswap)</Dropdown.Item>
+                          {coinSymbols.map(symbol => (
+                            <Dropdown.Item key={symbol} eventKey={symbol} active={this.state.tokenInSymbol === symbol} disabled={!this.state.whitelistedCoinSymbols.includes(symbol)}>{symbol}</Dropdown.Item>
+                          ))}
                         </DropdownButton>
                         <Form.Text>to</Form.Text>
                         <DropdownButton title={this.state.tokenOutSymbol} onSelect={(tokenOutSymbol, _) => this.setState({ tokenOutSymbol })} variant="dark">
-                          <Dropdown.Item eventKey="WETH" active={this.state.tokenOutSymbol === 'WETH'}>WETH (Wrapped ether)</Dropdown.Item>
+                          {coinSymbols.map(symbol => (
+                            <Dropdown.Item key={symbol} eventKey={symbol} active={this.state.tokenOutSymbol === symbol} disabled={!this.state.whitelistedCoinSymbols.includes(symbol)}>{symbol}</Dropdown.Item>
+                          ))}
                         </DropdownButton>
                       </Stack>
-                      <Form.Text>when the price of <span className="fw-bold">{this.state.tokenInSymbol}</span> is</Form.Text>
+                      <Form.Text>when the price of <span className="fw-bold">{this.state.tokenInSymbol}</span> becomes</Form.Text>
                       <Stack direction="horizontal" gap={1}>
                         <DropdownButton title={this.getOrderDirectionLabel(this.state.triggerDirection)} onSelect={(triggerDirection, _) => this.setState({ triggerDirection })} variant="dark">
                           <Dropdown.Item eventKey={2} active={this.state.triggerDirection === 2}>{this.getOrderDirectionLabel(2)}</Dropdown.Item>
                           <Dropdown.Item eventKey={1} active={this.state.triggerDirection === 1}>{this.getOrderDirectionLabel(1)}</Dropdown.Item>
                           <Dropdown.Item eventKey={0} active={this.state.triggerDirection === 0}>{this.getOrderDirectionLabel(0)}</Dropdown.Item>
                         </DropdownButton>
-                        <Form.Control type="number" placeholder="trigger price for token in" defaultValue={this.state.triggerPrice} onChange={e => this.setState({ triggerPrice: e.target.value })} />
+                        <Form.Control type="number" placeholder="example: 250.90" defaultValue={this.state.triggerPrice} onChange={e => this.setState({ triggerPrice: e.target.value })} />
+                        <Form.Text>USD$</Form.Text>
                       </Stack>
                     </Form.Group>
                   </Form>
                 </Card.Body>
                 <Card.Footer>
-                  <Button variant="primary" type="button" onClick={this.addOrder}>
+                  <Button variant="primary" type="button" onClick={this.tryAddOrder}>
                     Add order
                   </Button>
                 </Card.Footer>
